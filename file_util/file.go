a2e926ec504f54744dae8ff64dd5b15aea0bb888
lhh <776334655@qq.com>
2019-09-06 10:28:29 +0800

    init

--- file_util/file.go
+++ file_util/file.go
@@ -4,29 +4,34 @@ import (
 	"context"
 	"fmt"
 	"io"
-	"net/http"
+	"net/url"
 	"os"
 )
 
-func SaveFile(ctx context.Context, dir, name string, file io.ReadSeeker) (content string, err error) {
-	decByte := make([]byte, 512)
-	if _, err := file.Read(decByte); err != nil {
-		return "", err
+func SaveFile(ctx context.Context, dir, oriName string, file io.ReadSeeker) (content, key string, err error) {
+	fileContent, err := GetFileContent(ctx, file)
+	if err != nil {
+		return "", "", err
 	}
-	if _, err := file.Seek(0, io.SeekStart); err != nil {
-		return "", err
+	key, err = GetFileSha256(ctx, file)
+	if err != nil {
+		return "", "", err
 	}
-	contentType := http.DetectContentType(decByte)
-	fullPath := fmt.Sprintf("%v%v%v", dir, os.PathSeparator, name)
+	key = fmt.Sprintf("%s%s", key, url.PathEscape(oriName))
+	fullPath := fmt.Sprintf("%s%s%s", dir, string(os.PathSeparator), key)
 	f, err := os.OpenFile(fullPath, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0666)
+	if err != nil {
+		return "", "", err
+	}
+	defer f.Close()
 	_, err = io.Copy(f, file)
 	if err != nil {
 		err2 := os.Remove(fullPath)
-		return "", fmt.Errorf("%v %v", err, err2)
+		return "", "", fmt.Errorf("%v %v", err, err2)
 	}
-	return contentType, nil
+	return fileContent, key, nil
 }
 
-func RemoveFile(ctx context.Context, dir, name string) error {
-	return os.Remove(fmt.Sprintf("%v%v%v", dir, os.PathSeparator, name))
+func RemoveFile(ctx context.Context, dir, key string) error {
+	return os.Remove(fmt.Sprintf("%v%v%v", dir, os.PathSeparator, key))
 }
--- sfile.go
+++ sfile.go
@@ -3,6 +3,7 @@ package s_file
 import (
 	"context"
 	"io"
+	"os"
 	"s-file/file_util"
 )
 
@@ -11,12 +12,16 @@ type SFile struct {
 }
 
 func NewSFile(Dir string) *SFile {
+	err := os.MkdirAll(Dir, 0777)
+	if err != nil {
+		panic(err)
+	}
 	return &SFile{Dir: Dir}
 }
 
-func (a *SFile) SaveFile(ctx context.Context, name string, f io.ReadSeeker) (content string, err error) {
+func (a *SFile) SaveFile(ctx context.Context, name string, f io.ReadSeeker) (content, key string, err error) {
 	return file_util.SaveFile(ctx, a.Dir, name, f)
 }
-func (a *SFile) DeleteFile(ctx context.Context, name string) error {
-	return file_util.RemoveFile(ctx, a.Dir, name)
+func (a *SFile) DeleteFile(ctx context.Context, key string) error {
+	return file_util.RemoveFile(ctx, a.Dir, key)
 }
