234a07d396b6a33b0ae1a624b200a627f9aa8d87
lhh <776334655@qq.com>
2019-09-06 09:40:01 +0800

    init

--- sfile.go
+++ sfile.go
@@ -1,1 +1,22 @@
 package s_file
+
+import (
+	"context"
+	"io"
+	"s-file/file_util"
+)
+
+type SFile struct {
+	Dir string // 保存路径
+}
+
+func NewSFile(Dir string) *SFile {
+	return &SFile{Dir: Dir}
+}
+
+func (a *SFile) SaveFile(ctx context.Context, name string, f io.ReadSeeker) (content string, err error) {
+	return file_util.SaveFile(ctx, a.Dir, name, f)
+}
+func (a *SFile) DeleteFile(ctx context.Context, name string) error {
+	return file_util.RemoveFile(ctx, a.Dir, name)
+}
